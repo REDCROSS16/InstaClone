44801663cfa7d7bc8c6cfb272008c9371a89fd5c
redcross16 <belkill@mail.ru>
2020-11-25 21:25:19 +0300

    add log middleware

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-
-console.log('hello world');
-
-let a = 5;
-let b = 7;
-console.log("А в степени B:");
-console.log(a**b);
+app.use((req, res, next)=> {
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let data = `${hour}:${minutes}:${seconds} [method:${req.method}] [URL:${req.url}]`;
+    console.log(data);
+    fs.appendFile('server.log', data + "\n", function(){});
+    next();
+});
 
 // роутинг
-app.get('/', (req,res) => {
-    res.send('<h1>Hello WORLD</h1>');
-    
-    
+app.use('/', (req, res)=> {
+    res.sendFile(__dirname + '/template/index.html');
 });
+
 app.get('/json', (req, res) => {
     res.status(200).json({
         message:'Hello',
         age: 23,
         name: "Alex",
+    });
 });
-});
+
+app.get("/about", (req, res) => {
+    console.log(res.status);
+    res.send("<h1>О сайте</h1>");
+    });
+
+
 // прослушка порта 3000
 app.listen(3000, ()=> {
-    console.log('Example app listening on port 3000!');
-});
+    console.log('Смотрим порт 3000!');
+}); 
